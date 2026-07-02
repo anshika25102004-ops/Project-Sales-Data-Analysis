6f9bdff5519b30b5a8dd57af2278c07778ed8ef2
anshika25102004-ops <anshika25102004@gmail.com>
2026-07-02 12:22:35 +0530

    Create sales_analysis.py

---
+++ sales_analysis.py
@@ -0,0 +1,20 @@
+# Sales Data Analysis
+
+## Overview
+This project analyzes sales data using Python and Pandas.
+
+## Installation
+pip install -r requirements.txt
+
+## Run
+python sales_analysis.py
+
+## Technologies Used
+- Python
+- Pandas
+
+## Output
+- Total Revenue
+- Best Selling Product
+- Region-wise Sales
+- Average, Highest and Lowest Sales
